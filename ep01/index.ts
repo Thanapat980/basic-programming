d49ad711e4ac624410fc087b02363e5752db67a2
Thanapat980 <taxter2549@gmail.com>
2024-07-19 15:32:33 +0700

    เรียนประกาศตัวแปร

---
+++ ep01/index.ts
@@ -0,0 +1,10 @@
+const firstname: string = 'ธณพัฒน์ ศรีลาพัฒน์'
+
+let y: number = 10 
+y = 20
+
+let ต้องการกลับบ้าน: boolean = true
+
+console.log(firstname)
+console.log(y)
+console.log(ต้องการกลับบ้าน)
