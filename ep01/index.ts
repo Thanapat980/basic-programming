cf88601ff2dcb53755f093a2e87756759e01c983
Thanapat980 <taxter2549@gmail.com>
2024-07-19 16:09:44 +0700

    เปรียบดทียบA,B

--- ep01/index.ts
+++ ep01/index.ts
@@ -8,3 +8,10 @@ let ต้องการกลับบ้าน: boolean = true
 console.log(firstname)
 console.log(y)
 console.log(ต้องการกลับบ้าน)
+
+const a: number = 15
+const b: number = 51
+
+console.log(a===b)
+console.log(a>b)
+console.log(a<=b)
